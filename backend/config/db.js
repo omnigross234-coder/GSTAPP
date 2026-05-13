1d6a3a668e3818d101cce2fba6bf8d8c149e03f3
Prasad <182872040+Prasad-Kotewar@users.noreply.github.com>
2026-05-13 11:35:07 +0530

    fixed mysql production connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,20 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "gst_app"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect((err) => {
   if (err) {
-    console.log("Database connection failed ");
-    console.error(err);
+    console.error("Database connection failed:", err);
   } else {
-    console.log("MySQL Connected ");
+    console.log("MySQL Connected");
   }
 });
-
-module.exports = db;
