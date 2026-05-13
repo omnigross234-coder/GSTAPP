c0bd72ef2b460efee1e313936e614c4735b3cede
Prasad <182872040+Prasad-Kotewar@users.noreply.github.com>
2026-05-13 13:20:53 +0530

    fixed production api urls

--- gst_web_app/gst_invoice_web_app/src/features/auth/presentation/LoginPage.jsx
+++ gst_web_app/gst_invoice_web_app/src/features/auth/presentation/LoginPage.jsx
@@ -21,7 +21,7 @@ function LoginPage() {
 
     setLoading(true);
     try {
-      const res  = await fetch("https://backend-msas.onrender.com/login", {
+      const res  = await fetch("https://backend-msas.onrender.com/api/login", {
         method:  "POST",
         headers: { "Content-Type": "application/json" },
         body:    JSON.stringify({ username, password })
